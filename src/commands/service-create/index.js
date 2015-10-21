f98d32eb44676680c832f2bfbaf54abecd221df1
Andrew Munsell <andrew@wizardapps.net>
2015-10-20 19:26:29 -0700

    Load the service resources using the driver

--- src/commands/service-create/index.js
+++ src/commands/service-create/index.js
@@ -7,7 +7,10 @@ import {readFileSync, statSync, unlinkSync, writeFileSync} from 'fs';
 import {basename, join, resolve} from 'path';
 
 import {sync as mkpath} from 'mkpath';
+import {container} from 'needlepoint';
 import {v4 as uuid} from 'node-uuid';
+
+import Driver from 'polymerase-driver-aws-apigateway';
 
 export default function command(folder, options) {
 	var dir = folder && folder.trim().length ? resolve(folder, '.') : process.cwd();
@@ -28,44 +31,58 @@ export default function command(folder, options) {
 		return process.exit(1);
 	} catch(e) {
 		// The config file probably doesn't exist, so we can create it now.
-		mkpath(dir);
-
-		var defaultConfig = {
-			id: uuid(),
-			name: name,
-			version: '0.0.1',
-
-			type: 'aws-apigateway',
-
-			stages: {
-				default: 'default',
-				all: [
-					'default'
-				]
-			},
-
-			regions: [
-				'us-east-1'
-			],
-
-			repository: {},
-
-			manifest: {
-				version: '1.0.0'
-			}
-		};
-
-		if(options.description) {
-			defaultConfig.description = options.description;
-		}
-
-		writeFileSync(configPath, JSON.stringify(defaultConfig, null, 4), {
-			encoding: 'utf8',
-			flag: 'wx'
-		});
 	}
 
-	var config = readFileSync(configPath, { encoding: 'utf8' });
+	mkpath(dir);
 
-	console.log('Created a new service (' + name + ') in ' + dir);
+	var defaultConfig = {
+		id: uuid().substr(0, 13),
+		name: name,
+		version: '0.0.1',
+
+		type: 'aws-apigateway',
+
+		stages: {
+			default: 'default',
+			all: [
+				'default'
+			]
+		},
+
+		regions: [
+			'us-east-1'
+		],
+
+		repository: {},
+
+		manifest: {
+			version: '1.0.0'
+		}
+	};
+
+	if(options.description) {
+		defaultConfig.description = options.description;
+	}
+
+	writeFileSync(configPath, JSON.stringify(defaultConfig, null, 4), {
+		encoding: 'utf8',
+		flag: 'wx'
+	});
+
+	var config = JSON.parse(readFileSync(configPath, { encoding: 'utf8' }));
+
+	console.log('Created configuration file.');
+	console.log('Initializing the resources for your ' + config.type + ' service');
+	
+	Promise.resolve(null)
+	.then(function() {
+		var driver = container.resolve(Driver);
+
+		driver.setServiceContext(config);
+
+		return driver.createService();
+	})
+	.then(function() {
+		console.log('Created a new service (' + name + ') in ' + dir);
+	});
 };
